1a98a75e27de0e9f097ec28593a0774eb3de2902
Nataly Ramos <jnramosm@gmail.com>
2022-11-12 13:23:27 -0500

    Controllers

---
+++ Controllers/ventaController.js
@@ -0,0 +1,9 @@
+const prueba = (req, res) => {
+    res.send({
+        msg: "En está ruta gestionaremos todas las peticiones correspondiente al modelo de Venta"
+    })
+};
+
+export {
+    prueba
+};
